33e00f6bd0cb022de31e461088402e1e37c33869
Lucas <lucascardoso0221@gmail.com>
2024-09-14 23:30:39 -0300

    Adicionado movimento para torres e movimento de peões refatorado

--- Chessboard/Program.cs
+++ Chessboard/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ChessProject
+﻿using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ChessProject
 {
     internal class Chess
     {
@@ -7,7 +10,8 @@
             PrintChessboard();
 
             Console.WriteLine("");
-            PrintArr(WhitePawnMoves([1, 1]));
+
+            PrintArr(PawnMoves(1, 5));
         }
 
         
@@ -21,14 +25,14 @@
 
 
         static string[,] chessboard = {
-            { "wR", "wP", "", "", "", "", "bP", "bR" },
-            { "wN", "wP", "", "", "", "", "bP", "bN" },
-            { "wB", "wP", "", "", "", "", "bP", "bB" },
-            { "wQ", "wP", "", "", "", "", "bP", "bQ" },
-            { "wK", "wP", "", "", "", "", "bP", "bK" },
-            { "wB", "wP", "", "", "", "", "bP", "bB" },
-            { "wN", "wP", "", "", "", "", "bP", "bN" },
-            { "wR", "wP", "", "", "", "", "bP", "bR" },
+            { "wR", "wP", " ", " ", " ", " ", "bP", "bR" },
+            { "wN", "wP", " ", " ", " ", " ", "bP", "bN" },
+            { "wB", "wP", " ", " ", " ", " ", "bP", "bB" },
+            { "wQ", "wP", " ", " ", " ", " ", "bP", "bQ" },
+            { "wK", "wP", " ", " ", " ", " ", "bP", "bK" },
+            { "wB", "wP", " ", " ", " ", " ", "bP", "bB" },
+            { "wN", "wP", " ", " ", " ", " ", "bP", "bN" },
+            { "wR", "wP", " ", " ", " ", " ", "bP", "bR" },
         };
 
 
@@ -37,7 +41,7 @@
             Console.WriteLine("+----+----+----+----+----+----+----+----+");
             for (int i = chessboard.GetLength(0) - 1; i >= 0 ; i--) {
                 for (int j = 0; j < chessboard.GetLength(1); j++) {
-                    Console.Write($"| {(chessboard[j, i] == "" ? "  " : chessboard[j, i])} ");
+                    Console.Write($"| {(chessboard[j, i] == " " ? "  " : chessboard[j, i])} ");
                 }
                 Console.WriteLine("|");
                 Console.WriteLine("+----+----+----+----+----+----+----+----+");
@@ -45,69 +49,104 @@
         }
 
 
-#region Piece Legal Moves
+#region Piece Moves
 
-        static List<int[]> WhitePawnMoves(int[] coord)
+        static void AddMoves (int startX, int startY, int deltaX, int deltaY, List<int[]> list)
+        {
+            char opositeColor = chessboard[startX, startY][0] == 'w' ? 'b' : 'w';
+            
+            int currentX = startX;
+            int currentY = startY;
+
+            while (true)
+            {
+                currentX += deltaX;
+                currentY += deltaY;
+
+                // Out of bounds
+                if (currentX < 0 || currentX > 7 || currentY < 0 || currentY > 7) { break; }
+
+                string coordinate = chessboard[currentX, currentY];
+
+                if (coordinate == " ") { list.Add([currentX, currentY]); }
+                else if (coordinate[0] == opositeColor) { list.Add([currentX, currentY]); break; }
+                else { break; }
+            }
+        }
+
+
+        static List<int[]> PawnMoves(int x, int y)
         {
             List<int[]> moves = new List<int[]>();
 
-            bool moved = coord[1] != 1;
+            int direction = chessboard[x, y][0] == 'w' ? 1 : -1;
+            char opositeColor = chessboard[x, y][0] == 'w' ? 'b' : 'w';
 
-            try { 
-                if (!moved) {
-                    for (int i = 1; i <= 2; i++) {
-                        if (chessboard[coord[0], coord[1] + i] == "") {
-                            moves.Add([coord[0], coord[1] + i]);
-                        }
-                    }
+            void AddPawnMove (int x, int newY)
+            {
+                if (newY >= 0 && newY <= 7 && chessboard[x, newY] == " ")
+                {
+                    moves.Add([x, newY]);
                 }
-                else {
-                    if (chessboard[coord[0], coord[1] + 1] == "") {
-                        moves.Add([coord[0], coord[1] + 1]);
-                    }
-                }
+            }
 
-                for (int i = -1; i <= 1; i += 2) {
-                    if (chessboard[coord[0] + i, coord[1] + 1][0] == 'b') {
-                        moves.Add([coord[0] + i, coord[1] + 1]);
-                    }
-                }
+            // Move one square
+            int oneSqrY = y + direction;
+            AddPawnMove(x, oneSqrY);
 
-            } catch {};
+            // Move two squares
+            int twoSqrY = y + 2 * direction;
+            if (y == 1 && direction == 1 || y == 6 && direction == -1)
+            {
+                AddPawnMove(x, twoSqrY);
+            }
+
+            // Captures
+            int[] capturesX = { x + 1, x - 1 };
+            foreach (int captureXPos in capturesX)
+            {
+                if (captureXPos >= 0 && captureXPos <= 7)
+                {
+                    int captureYPos = oneSqrY;
+                    if (chessboard[captureXPos, oneSqrY][0] == opositeColor) { moves.Add([captureXPos, captureYPos]); }
+                }
+            }
 
             return moves;
         }
 
 
-        static List<int[]> BlackPawnMoves(int[] coord)
+        static List<int[]> RookMoves(int x, int y) {
+            List<int[]> moves = new List<int[]>();
+
+            // Add movement for vertical direction
+            AddMoves(x, y, 0, 1, moves);
+            AddMoves(x, y, 0, -1, moves);
+
+            // Add movement for horizontal direction
+            AddMoves(x, y, 1, 0, moves);
+            AddMoves(x, y, -1, 0, moves);
+
+            return moves;
+        }
+
+
+        static List<int[]> OLDWhitePawnMoves(int x, int y)
         {
             List<int[]> moves = new List<int[]>();
 
-            bool moved = coord[1] != 6;
+            // out of bound
+            if (y == 7) { return moves; }
 
-            try {
-                if (!moved) {
-                    for (int i = 1; i <= 2; i++) {
-                        if (chessboard[coord[0], coord[1] - i] == "") {
-                            moves.Add([coord[0], coord[1] - i]);
-                        }
-                    }
-                }
-                else {
-                    if (chessboard[coord[0], coord[1] - 1] == "")
-                    {
-                        moves.Add([coord[0], coord[1] - 1]);
-                    }
-                }
+            // pawn hasn't moved
+            if (y == 1 & chessboard[x, y + 2] == " ") { moves.Add([x, y + 2]); }
 
-                for (int i = -1; i <= 1; i += 2) {
-                    if (chessboard[coord[0] + i, coord[1] - 1][0] == 'w') {
-                        moves.Add([coord[0] + i, coord[1] - 1]);
-                    }
-                }
+            // pawn move
+            if (chessboard[x, y + 1] == " ") { moves.Add([x, y + 1]); }
 
-            }
-            catch {};
+            // pawn capture
+            if (x < 7 & chessboard[x + 1, y + 1][0] == 'b') { moves.Add([x + 1, y + 1]); }
+            if (x > 0 & chessboard[x - 1, y + 1][0] == 'b') { moves.Add([x - 1, y + 1]); }
 
             return moves;
         }
